4914fd66b63ca95a7305818b1df8a377473a4c41
Prince Andrew <prince.andrew@ripotek.com>
2025-11-23 22:25:32 -0700

    Updated subscribe button in Blog page

--- app/layout.js
+++ app/layout.js
@@ -21,11 +21,12 @@ export const metadata = {
     apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 5,
-  },
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
   themeColor: '#0d9488',
 }
 
